305d5caea2a36935634e3912ffde07f3ba6f1ecf
bunnyxt <bunnyxt@outlook.com>
2021-05-24 23:37:18 +0800

    feat: WIP 剥离视频逻辑

--- functions/update_data/index.js
+++ functions/update_data/index.js
@@ -9,18 +9,18 @@ const getDbConn = async () => {
   })
 }
 
-async function getMids(conn) {
+async function getUpMidList(conn) {
   let sql = `
     select v.aid, mid, hasstaff
     from tdd_video v left join tdd_video_record r on v.laststat = r.id 
     where v.code = 0 && v.state = 0 && r.view > 1000000;`;
   let results = await conn.query(sql);
 
-  let mids = [];
+  let upMidList = [];
   const hasStaffVideoAids = [];
   for (const r of results) {
     if (r.hassstaff === 0) {
-      mids.push(r.mid);
+      upMidList.push(r.mid);
     } else {
       hasStaffVideoAids.push(r.aid);
     }
@@ -30,23 +30,20 @@ async function getMids(conn) {
     sql = 'select mid from tdd_video_staff where aid = ?;';
     results = await conn.query(sql, aid);
     for (const r of results) {
-      mids.push(r.mid);
+      upMidList.push(r.mid);
     }
   }
 
-  mids = [...new Set(mids)];
-
-  return mids;
+  return [...new Set(upMidList)];
 }
 
-async function getUpData(mids, conn) {
-  const upData = [];
-  const videoStaffCache = {};
-  for (const mid of mids) {
+async function getUpInfoList(upMidList, conn) {
+  const upInfoList = [];
+  for (const mid of upMidList) {
     let sql = `
-      select m.mid, sex, name, face, sign, video_count, 
-        aid, bvid, videos, pic, title, pubdate, \`desc\`, tags, hasstaff
-      from tdd_member m left join tdd_video v on m.last_video = v.id 
+      select m.mid, sex, name, face, sign, video_count,
+        fr.added as follower_added, fr.follower
+      from tdd_member m left join tdd_member_follower_record fr on m.last_follower = fr.id
       where m.mid = ?;`;
     let result = await conn.query(sql, mid);
     result = result[0];
@@ -54,66 +51,56 @@ async function getUpData(mids, conn) {
       console.log(`WARNING: mid ${mid} last_video is NULL, this should not happen, skip.`);
       continue;
     }
-    const data = {
+    upInfoList.push({
       mid,
       sex: result.sex,
       name: result.name,
       face: result.face,
       sign: result.sign,
       video_count: result.video_count,
-      last_video: {
-        aid: result.aid,
-        bvid: result.bvid,
-        videos: result.videos,
-        pic: result.pic,
-        title: result.title,
-        pubdate: result.pubdate,
-        desc: result.desc,
-        tags: result.tags,
-      },
-    };
-    if (result.hasstaff === 0) {
-      data.last_video.staff = null;
-    } else {
-      if (!videoStaffCache[data.last_video.aid]) {
-        const sql = `
-          select s.mid, s.title, m.name, m.face, m.sex, m.sign 
-          from tdd_video_staff s left join tdd_member m on s.mid = m.mid 
-          where aid = ?;`
-        const staffResults = await conn.query(sql, data.last_video.aid);
-        const staff = [];
-        for (const r of staffResults) {
-          staff.push({
-            mid: r.mid,
-            title: r.title,
-            name: r.name, 
-            face: r.face,
-            sex: r.sex,
-            sign: r.sign,
-          });
-        }
-        videoStaffCache[data.last_video.aid] = staff;
-      }
-      data.last_video.staff = videoStaffCache[data.last_video.aid];
-    }
-    upData.push(data);
+      follower: result.follower,
+      follower_added: result.follower_added,
+    });
+    // const videoStaffCache = {};
+    // if (result.hasstaff === 0) {
+    //   data.last_video.staff = null;
+    // } else {
+    //   if (!videoStaffCache[data.last_video.aid]) {
+    //     const sql = `
+    //       select s.mid, s.title, m.name, m.face, m.sex, m.sign 
+    //       from tdd_video_staff s left join tdd_member m on s.mid = m.mid 
+    //       where aid = ?;`
+    //     const staffResults = await conn.query(sql, data.last_video.aid);
+    //     const staff = [];
+    //     for (const r of staffResults) {
+    //       staff.push({
+    //         mid: r.mid,
+    //         title: r.title,
+    //         name: r.name, 
+    //         face: r.face,
+    //         sex: r.sex,
+    //         sign: r.sign,
+    //       });
+    //     }
+    //     videoStaffCache[data.last_video.aid] = staff;
+    //   }
+    //   data.last_video.staff = videoStaffCache[data.last_video.aid];
+    // }
   }
 
-  upData.sort((a, b) => a.last_video.pubdate - b.last_video.pubdate);
-
-  return upData;
+  return upInfoList;
 }
 
 exports.main = async function () {
   const conn = await getDbConn();
 
   // 步骤1: 获取满足条件的视频的创作者们
-  const mids = await getMids(conn);
+  const upMidList = await getUpMidList(conn);
 
-  // 步骤2: 获取这些创作者们的个人信息和视频数据
-  const upData = await getUpData(mids, conn);
+  // 步骤2: 获取这些创作者们的个人信息
+  const upInfoList = await getUpInfoList(upMidList, conn);
 
   conn.end();
 
-  return upData;
+  return upInfoList;
 };
